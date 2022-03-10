bb6aef6080c1888bdf52863e607c564673fa7dd2
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-03-10 21:38:39 +0600

    Revert "user image"
    This reverts commit dca6843f83c3c1d0988d4c33b6fc1683b4337037.

--- js/app.js
+++ js/app.js
@@ -53,7 +53,6 @@ const switchTab = (id) => {
 
 const createPost = (post) => {
     const image = post.image;
-    const userImage = post.userImage;
     const div = document.createElement( "article" );
     div.classList.add( "post" );
     div.innerHTML = `
@@ -64,7 +63,7 @@ const createPost = (post) => {
                     target="_blank"
                     class="post__avatar"
                   >
-                    <img src="${userImage}" alt="User Picture" />
+                    <img src="${image}" alt="User Picture" />
                   </a>
                   <a href="#" class="post__user">phero</a>
                 </div>
